f5bcc06b7a93fc61e293f2912b93e5f5b877bcbd
Jayavardhan J <101786842+jayavardhanjv@users.noreply.github.com>
2023-04-27 11:42:40 +0530

    Add files via upload

---
+++ Implementation-of-data-structure-using-Music-Player-main/js/script.js
@@ -0,0 +1,43 @@
+
+
+
+
+// Splash screen animation 
+
+console.log("Script.js is running!!!")
+var splashScreenTime = 4000;
+
+function someFoo() {
+}
+
+
+function pageLoaded(){
+
+  setTimeout(() => 
+  {
+      localStorage.setItem("email","true")
+  }, 4000);
+  if (localStorage.getItem("email") == null) 
+  {
+    var animation =bodymovin.loadAnimation({
+      container : document.getElementById('animate'),
+      renderer:'svg',
+      loop:true,
+      autoplay:true,
+      path:'data.json'
+    })
+    // console.log("Animate Function called");  
+    const element = document.getElementById("animate");
+    setTimeout(() => {
+      element.remove();
+    }, splashScreenTime);
+  }else{  
+    // console.log(localStorage.getItem("email"));
+    // Do nothing
+  }
+}
+
+// window.addEventListener("beforeunload",()=>localStorage.removeItem('email'));
+
+
+
